d43aa51731842855c25f88cd9a88d9c9c3a5b27a
macbre <maciej.brencz@gmail.com>
2012-09-09 17:52:14 +0200

    Generuj dane o przystankach końcowych dla linii autobusowych

--- petle.js
+++ petle.js
@@ -1,5 +1,5 @@
 /**
- * Skrypt generujący dane o przystankach końcowych linii tramwajowych
+ * Skrypt generujący dane o przystankach końcowych linii tramwajowych i autobusowych
  */
 var http = require('http'),
 	fs = require('fs');
@@ -7,46 +7,74 @@ var http = require('http'),
 var routes = {},
     re = />([^<]+)<\/a><\/li><\/ul>/g;
 
-for (var l=1; l < 30; l++) {
-	(function(line) {
-		var url = {
-			host: '193.218.154.93',
-			path: '/dbServices/gtfs-ztm/route_directions.html.php?route_name=' + line + '&agency_name=ZTM_MPK'
-		},
-		page = '';
-		
-		http.get(url, function(res) {
-			res.on("data", function(chunk) {
-				page += chunk;
-			});
+var l, lines = [];
 
-			res.on('end', function() {
-				var matches = page.match(re) || []
+// linie tramwajowe
+for (l=1; l<30; l++) {
+	lines.push(l);
+}
+lines.push('N21');
 
-				matches.forEach(function(match) {
-					var stop = match.substr(1, match.length - 15);
+// linie autobusowe (dzienne)
+for (l=40; l<100; l++) {
+	lines.push(l);
+}
 
-					// ucfirst()
-					stop = stop[0] + stop.substring(1).toLowerCase();
+// linie autobusowe (nocne)
+for (l=231; l<255; l++) {
+	lines.push(l);
+}
 
-					// porządki
-					stop = stop.
-						replace("Os. sobieskiego", "Osiedle Jana III Sobieskiego").
-						replace("Zajezdnia głogowska", "Zajezdnia Głogowska");
-
-					// dodaj pętle do danych linii
-					routes[line] = routes[line] || [];
-					routes[line].push(stop);
-
-					console.log('#' + line + ': ' + stop);
-
-					// katualizuj "bazę"
-					fs.writeFileSync('db/petle.json', JSON.stringify(routes));
-				});
-			});
-		}).on('error', function(e) {
-			console.log(e);
+lines.forEach(function(line) {
+	var url = {
+		host: '193.218.154.93',
+		path: '/dbServices/gtfs-ztm/route_directions.html.php?route_name=' + line + '&agency_name=ZTM_MPK'
+	},
+	page = '';
+	
+	http.get(url, function(res) {
+		res.on("data", function(chunk) {
+			page += chunk;
 		});
 
-	})(l);
-}
+		res.on('end', function() {
+			var matches = page.match(re) || []
+
+			matches.forEach(function(match) {
+				var stop = match.substr(1, match.length - 15).trim();
+
+				// ucfirst()
+				stop = stop[0] + stop.substring(1).toLowerCase();
+
+				// porządki
+				stop = stop.
+					trim().
+					replace("Os. sobieskiego", "Osiedle Jana III Sobieskiego").
+					replace(/pl\. /i, "Plac ").
+					replace(/os\. /i, "Osiedle ").
+					replace("os.", "Osiedle ").
+					// os. batorego ii -> os. batorego II
+					replace(/\si+$/g, function(match) {
+						return match.toUpperCase();
+					});
+
+				// ucfirst()
+				stop = stop.replace(/[\s|\/][\w\W]/g, function(match) {
+					return match.toUpperCase()
+				});
+
+				// dodaj pętle do danych linii
+				routes[line] = routes[line] || [];
+				routes[line].push(stop);
+
+				console.log('#' + line + ': ' + stop);
+
+				// katualizuj "bazę"
+				fs.writeFileSync('db/petle.json', JSON.stringify(routes));
+			});
+		});
+	}).on('error', function(e) {
+		console.log(e);
+	});
+
+});
