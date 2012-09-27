3e59bc1bf2d160a756ba263db172c426dd9ac210
macbre <maciej.brencz@gmail.com>
2012-09-27 11:47:37 +0200

    Dodatkowe kategorie dla zdjęć z 11ujęć

--- 11ujec_import.js
+++ 11ujec_import.js
@@ -4,12 +4,16 @@
 var bot = require('../lib/bot').bot,
 	client = new bot('config.js');
 
-var STREET = 'Ulica Wiatraczna',
-	URL = 'http://11ujec.blogspot.com/2012/05/ulica-wiatraczna.html',
+var STREET = 'Ulica Rzeczańska',
+	URL = 'http://11ujec.blogspot.com/2012/05/ulica-rzeczanska.html',
 	IMAGES = [
-		'http://4.bp.blogspot.com/-Rj-7oQJD9Ds/T6aZNqgLYqI/AAAAAAAAJr8/mOBtpyV_8AY/s1600/IMG_6217.jpg',
-		'http://4.bp.blogspot.com/-HojAtiJj9K0/T6aZVHmrNAI/AAAAAAAAJsk/FygBisOitFk/s1600/IMG_6224.jpg',
-		'http://1.bp.blogspot.com/-qDe_G9CWifA/T6aZYBmv8EI/AAAAAAAAJs0/nvDrJfDf4bI/s1600/IMG_6227.jpg'
+		'http://3.bp.blogspot.com/-9MHvoWj0j1I/T7FQsH_0UZI/AAAAAAAAJ7c/zmN5_u8yBqk/s1600/IMG_6295.jpg',
+		'http://2.bp.blogspot.com/-HROHvjjn1es/T7FQjP_5d0I/AAAAAAAAJ6k/lssq8o76enE/s1600/IMG_6286.jpg',
+		'http://3.bp.blogspot.com/-TnPJn-_5jrg/T7FQwixBiLI/AAAAAAAAJ70/G_NSG5HdJN4/s1600/IMG_6310.jpg'
+	],
+	CATEGORIES = [
+		STREET,
+		'Żegrze'
 	];
 
 client.logIn(function() {
@@ -23,9 +27,11 @@ client.logIn(function() {
 		// dodaj zdjęcia
 		client.uploadByUrl(filename, image, 'Import zdjęć ze strony 11ujec.blogspot.com', function(res) {
 			console.log('Upload ' + filename + ' zakończony');
-			
+
 			// oznacz zdjęcia szablonem autora + kategoria
-			var content = '{{Fotografie 11 ujęć}}\n\n[[Kategoria:' + STREET + ']]';
+			var categories = '[[Kategoria:' + CATEGORIES.join(']]\n[[Kategoria:')  + ']]',
+				content = '{{Fotografie 11 ujęć}}\n\n' + categories;
+
 			client.edit('Plik:' + filename, content, 'Oznaczanie zdjęć ze strony 11ujec', function(res) {
 				console.log('Plik ' + filename + ' oznaczony');
 			});
