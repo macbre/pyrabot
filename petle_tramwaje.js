8a97e3ddac47f2e3c66984810c2a3d72f0b9d970
macbre <maciej.brencz@gmail.com>
2012-10-06 12:58:06 +0200

    Aktualizuj dane o pętlach linii tramwajowych i autobusowych

--- petle_tramwaje.js
+++ petle_tramwaje.js
@@ -7,48 +7,59 @@ var SUMMARY = 'Zaktualizowano dane o przystankach końcowych';
 // odczytaj bazę pętli
 var db = JSON.parse(fs.readFileSync('db/petle.json'));
 
+function updateLine(pageTitle) {
+	var page = {title: pageTitle};
+
+	var line = page.title.substring(20), // usuń prefix "Linia tramwajowa/autobusowa nr "
+		stops = db[line];
+
+	console.log("\n" + page.title + ' (#' + line + ')');
+
+	if (typeof stops === 'undefined') {
+		console.log('>>> brak danych!');
+		return;
+	}
+
+	// linie autobusowa z jedną pętlą (62, 89, 94, 96, ...)
+	if (stops.length === 1) {
+		stops[1] = stops[0];
+	}
+
+	console.log(stops);
+
+	client.getArticle(page.title, function(content) {
+		// aktualizuj infobox
+		content = content.replace(/\|pętla1\s?=[^|]+/, "|pętla1=" + stops[0] + "\n");
+		content = content.replace(/\|pętla2\s?=[^|]+/, "|pętla2=" + stops[1] + "\n");
+
+		//console.log('\n\n================================\n' + page.title + '\n================================');
+		//console.log(content);
+
+		// zapisz zmiany
+		client.edit(page.title, content, SUMMARY, function(data) {
+			console.log('\n\n> ' + page.title + ' zaktualizowana!');
+		});
+	});
+}
+
 client.logIn(function() {
-	client.getPagesInCategory('Linie tramwajowe', function(pages) {
+	client.getPagesByPrefix('Linia tramwajowa nr', function(pages) {
 		pages && pages.forEach(function(page) {
 			if (page.ns != 0) {
 				return;
 			}
 
-			if (page.title.indexOf('Linia tramwajowa nr') != 0) {
+			updateLine(page.title);
+		});
+	});
+
+	client.getPagesByPrefix('Linia autobusowa nr', function(pages) {
+		pages && pages.forEach(function(page) {
+			if (page.ns != 0) {
 				return;
 			}
 
-			client.getArticle(page.title, function(content) {
-				var line = page.title.substring(20),
-					stops = db[line];
-
-				if (typeof stops === 'undefined') {
-					return;
-				}
-
-				console.log("\n" + page.title + ' (#' + line + ')');
-				console.log(stops);
-
-				// aktualizuj infobox
-				content = content.replace(/\|pętla1\s?=[^|]+/, "|pętla1=" + stops[0] + "\n");
-				content = content.replace(/\|pętla2\s?=[^|]+/, "|pętla2=" + stops[1] + "\n");
-
-				console.log('\n\n================================\n' + page.title + '\n================================');
-				console.log(content);
-
-				// zapisz zmiany
-				/**/
-				try {
-					client.edit(page.title, content, SUMMARY, function(data) {
-						console.log('\n\n> ' + page.title + ' edited!');
-					});
-				} catch(e) {
-					console.log(e);
-				}
-				/**/
-			});
+			updateLine(page.title);
 		});
-
-		console.log("\n\nDone");
 	});
 });
