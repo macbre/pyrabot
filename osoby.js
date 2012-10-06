8e3fc1a04d7936ea9c5e9c4721d2aa7fa0948212
macbre <maciej.brencz@gmail.com>
2012-10-06 12:06:39 +0200

    Osoby: używaj metody getTemplateParamFromXml do pobierania danych z infoboxów

--- osoby.js
+++ osoby.js
@@ -1,29 +1,15 @@
-var fs = require('fs');
-var bot = require('../lib/bot').bot;
-
-var client = new bot('config.js');
-
-var urRegExp = /<part><name>data_ur\s*<\/name>=<value>([^>]+)<\/value>/,
-    smRegExp = /<part><name>data_sm\s*<\/name>=<value>([^>]+)<\/value>/;
+var fs = require('fs'),
+	bot = require('../lib/bot').bot,
+	client = new bot('config.js');
 
 var persons = [];
 
 client.getPagesInCategory('Osoby', function(pages) {
-	var n = 0;
-
 	pages.forEach(function(page) {
-		//if (n++ > 5) return;
-
 		client.getArticle(page.title, function(content) {
 			client.expandTemplates(content, page.title, function(tmpl) {
-				//console.log('\n\n==' + page.title + '==');
-				//console.log(tmpl);
-
-				var ur = tmpl.match(urRegExp),
-					sm = tmpl.match(smRegExp);
-
-				ur = ur && ur[1].trim() || '';
-				sm = sm && sm[1].trim() || '';
+				var ur = client.getTemplateParamFromXml(tmpl, 'data_ur') || '',
+					sm = client.getTemplateParamFromXml(tmpl, 'data_sm') || '';
 
 				// pobierz tylko rok + usuń brackety linków
 				ur = parseInt(ur.split(' ').pop().replace(/\[|\]/g, ''), 10) || '';
