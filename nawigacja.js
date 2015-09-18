95ec6016495e97d075933d0e5571e853728e3454
Maciej Brencz <maciej.brencz@gmail.com>
2015-09-18 20:50:30 +0200

    nawigacja.js: aktualizacja skryptu

--- nawigacja.js
+++ nawigacja.js
@@ -1,39 +1,42 @@
+#!/usr/bin/env node
 /**
  * Skrypt dodający szablon nawigacji po artykułach w danej kategorii
  */
-
-var bot = require('../lib/bot').bot,
+var bot = require('nodemw'),
 	client = new bot('config.js');
 
-var SUMMARY = 'Dodano nawigację',
-	templateName = 'Nawigacja Kościoły',
-	category = 'Kościoły';
+var SUMMARY = 'Dodałem nawigację',
+	templateName = 'Nawigacja parki i skwery',
+	category = 'Parki i skwery';
 
-client.logIn(function(data) {
-	client.getPagesInCategory(category, function(pages) {
+client.log('Dodaję nawigację "{{%s}}" do stron w kategorii "%s"...', templateName, category);
+
+client.logIn(function(err, data) {
+	client.getPagesInCategory(category, function(err, pages) {
 		pages && pages.forEach(function(page) {
 			// tylko NS_MAIN
 			if (page.ns != 0) {
 				return;
 			}
 
-			client.getArticle(page.title, function(content) {
+			client.getArticle(page.title, function(err, content) {
 				// dodano już szablon
 				if (content.indexOf(templateName) > -1) {
 					return;
 				}
 
+				var orig = content;
+
 				// dodaj szablon
 				content += "\n\n{{" + templateName + "}}";
 
-				console.log('Dodaję nawigację do ' + page.title + '...');
+				client.log('Dodaję nawigację do ' + page.title + '...');
 
-				//console.log('\n\n================================\n' + page.title + '\n================================');
-				//console.log(content);
+				client.log(client.diff(orig, content));
 
 				// and save it
-				client.edit(page.title, content, SUMMARY, function(data) {
-					console.log(page.title + ' zapisany');
+				client.edit(page.title, content, SUMMARY, function(err, data) {
+					if (!err) client.log(page.title + ' zapisany');
 				});
 			});
 		});
