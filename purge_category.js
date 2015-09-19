92b2f0e5fea6be4e828306698b8d5610de2d2236
Maciej Brencz <maciej.brencz@gmail.com>
2015-09-19 15:30:23 +0200

    Dodaj skrypt purge_category.js

---
+++ purge_category.js
@@ -0,0 +1,33 @@
+#!/usr/bin/env node
+/**
+ * Skrypt odświeżający strony w podanej kategorii
+ */
+var bot = require('nodemw'),
+	client = new bot('config.js');
+
+var CATEGORY = process.argv[2] || '';
+
+if (CATEGORY === '') {
+	console.log('Podaj nazwę kategorii');
+	process.exit(1);
+}
+
+client.log('Purdżuję artykuły w kategorii "%s"...', CATEGORY);
+
+client.getPagesInCategory(CATEGORY, function(err, pages) {
+	if (err) return;
+
+	var pageIds = pages.
+		filter(function(page) {
+			return page.ns === 0; // NS_MAIN
+		}).
+		map(function(page) {
+			return page.pageid;
+		});
+
+	client.purge(pageIds, function(err, data) {
+		if (err) client.log(err);
+		//console.log(data);
+	});
+});
+
