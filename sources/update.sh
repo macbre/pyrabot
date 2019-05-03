5014090c605c7aabe1d5436b8884c59274c21221
macbre <maciej.brencz@gmail.com>
2019-05-03 16:02:38 +0200

    geopoz | pobieraj nowy plik CSV
    https://www.geopoz.pl/bip/index.php?t=200&id=3366

--- sources/update.sh
+++ sources/update.sh
@@ -3,27 +3,9 @@
 echo "Aktualizuje dane"
 
 # baza Geopozu
-echo "\n> Pobieram spis obiektów w bazie Geopozu..."
-curl -s "http://sip.geopoz.pl/data/ulice_adresy/ulice_pobieranie.php" | iconv -f windows-1250 -t utf8  | dos2unix > geopoz.csv
-
-# numeracja ulic
-echo "\n> Pobieram dane o numeracji ulic..."
-curl -s "http://sip.geopoz.pl/data/ulice_adresy/adresy_pobieranie_obowiazujace.php" |  iconv -f windows-1250 -t utf8  | dos2unix \
-	> ulice-numeracja.csv
-
-# długości ulic
-echo "\n> Pobieram dane o ulicach w układzie podstawowym..."
-curl -s "http://www.zdm.poznan.pl/content/pliki/Uk%C5%82ad_podstawowy_wykaz_stan_na_I_2014.xls" > tmp.xls && xls2csv tmp.xls > ulice-ztm-podstawowy.csv
-
-echo "\n> Pobieram dane o ulicach w układzie uzupełniającym..."
-curl -s "http://www.zdm.poznan.pl/content/pliki/Ukl%20uzupelniajacy%20wg%20stanu%20na%20dzien%202_11_2012r.xls" > tmp.xls && xls2csv tmp.xls > ulice-ztm-uzupelniajacy.csv
-
-echo "\n> Pobieram dane o ulicach wewnętrznych..."
-curl -s "http://www.zdm.poznan.pl/content/pliki/Uk%C5%82ad_uzupe%C5%82niaj%C4%85cy_wykaz_stan_na_I_2014.xls" > tmp.xls && xls2csv tmp.xls > ulice-ztm-wewnetrzne.csv
-
-# kody pocztowe
-echo "\n> Pobieram dane o kodach pocztowych..."
-./scripts/kody-pocztowe.py
+# https://www.geopoz.pl/bip/index.php?t=200&id=3366
+echo "\n> Pobieram spis ulic w bazie Geopozu..."
+curl -s "https://www.geopoz.pl/bip/index.php?t=210&id=3372&mode=a" | iconv -f windows-1250 -t utf-8 | dos2unix > geopoz.csv
 
 # komunikacja miejska
 echo "\n> Pobieram dane GTFS od ZTM..."
